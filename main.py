9c9057dc11dfe3d23039fa8fe83bd5c3a8a3e742
AnGeLL1337 <matej@samolej.sk>
2024-05-23 09:28:09 +0200

    Pridané external_ids

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ from selenium.webdriver import Chrome, ChromeOptions
 from bs4 import BeautifulSoup
 import re
 import json
-import time
-import os
+import uuid
 
-import getpass
+
+def generate_uuid() -> str:
+    return str(uuid.uuid4())
 
 def get_id(url1, driver) -> list:
-    
+
     driver.get(url1)
     ids = driver.page_source
     driver.quit()
-    
+
     return ids
 
 def remove_keyword(string, keyword):
@@ -31,7 +32,8 @@ def remove_chars(string, chars):
             result += char
     return result
 
-def get_data(finalurl, driver2) -> list:
+
+def get_data(finalurl, driver2) -> tuple:
     #print(finalurl)
     driver2.get(finalurl)
     data = BeautifulSoup(driver2.page_source, 'html.parser')
@@ -94,9 +96,9 @@ def get_data(finalurl, driver2) -> list:
         faculty = elems[8].text.strip()
         taught_groups = data.find_all("div", id="StudiumSkupina")
         taught_group = [group.text.strip() for group in taught_groups]
-    
+
     return name, title, rank, department, email, phone, mobile, data_box, campus, building_room, faculty, taught_group
-    
+
 def main():
     with open("personal.json") as f:
         personal = json.load(f)
@@ -106,65 +108,67 @@ def main():
     url2 = personal['url2']
     user = personal['user']
     password = personal['password']
-    
+
     driver = Chrome()
     driver.get(login1Url)
     driver.get(login1Url)
-    
+
     username_field = driver.find_element(By.ID, "floatingInput")
     password_field = driver.find_element(By.ID, "floatingPassword")
-    
+
     username_field.send_keys(user)
     password_field.send_keys(password)
-         
+
     submit = driver.find_element(By.NAME, "button")
     submit.click()
-    
+
     ids = get_id(url1, driver)
-    
+
     pattern = re.compile(r'"teachers":(.*?)],"classrooms"')
     matches = pattern.findall(ids)
-        
+
     if matches:
         userID = matches[0].strip()
         print("Match found.")
     else:
         print("Not match found.")
-        
+
     userIDSplits = userID.split("},{")
-    
+
     with open("ids.txt", "a") as f:
         for userIDSplit in userIDSplits:
             user_id, user_surname, user_name = userIDSplit.split(",")
-            user_id  = remove_keyword(user_id, '"id":')
-            user_id  = remove_chars(user_id, '[{')
+            user_id= remove_keyword(user_id, '"id":')
+            user_id= remove_chars(user_id, '[{')
             f.write(user_id + "\n")
-    
+
     print("IDs have been written.")
-    
-    driver2 = Chrome()    
+
+    driver2 = Chrome()
     driver2.get(login2Url)
     driver2.get(login2Url)
-    
+
     username_field = driver2.find_element(By.ID, "floatingInput")
     password_field = driver2.find_element(By.ID, "floatingPassword")
-    
+
     username_field.send_keys(user)
     password_field.send_keys(password)
-         
+
     submit = driver2.find_element(By.NAME, "button")
     submit.click()
-    
+
     data = {}
-    with open("data.json", "w") as f:
+    with open("systemdata.json", "w") as f:
         f.write("[\n")
-    
+
+    external_ids = []
+
     with open("ids.txt", "r") as f:
         for line in f:
             value = line.strip()
             finalurl = url2 + value
             name, title, rank, department, email, phone, mobile, data_box, campus, building_room, faculty, taught_group = get_data(finalurl, driver2)
-            
+
             data = {
                 "ID" : value,
                 "Jméno" : name,
@@ -181,12 +185,25 @@ def main():
                 "Seznam vyučovaných skupin" : taught_group
             }
 
-            with open("data.json", "a", encoding="utf-8") as f:
+            with open("systemdata.json", "a", encoding="utf-8") as f:
                 f.write(json.dumps(data, ensure_ascii=False, indent=2) + ",\n")
-    
-    
+
+
+            external_id = {
+                "id": generate_uuid(),
+                "name": name,
+                "name_en": name,
+                "urlformat": finalurl,
+                "external_id": value
+            }
+            external_ids.append(external_id)
+
+    with open("external_ids.json", "w", encoding="utf-8") as f:
+        json.dump(external_ids, f, ensure_ascii=False, indent=2)
+
+
     # --- Part of code that erases last ',' between data of each user if there is no next
-    with open("data.json", "r", encoding="utf-8") as f:
+    with open("systemdata.json", "r", encoding="utf-8") as f:
         lines = f.readlines()
 
     if lines:
@@ -194,10 +211,12 @@ def main():
         if last_line.endswith(","):
             lines[-1] = last_line[:-1] + "\n"
 
-    with open("data.json", "w", encoding="utf-8") as f:
+    with open("systemdata.json", "w", encoding="utf-8") as f:
         f.writelines(lines)
         f.write("]\n")
     # ---
+
+    print("Teachers have been written.")
 
     driver2.quit()
 
